79434bdc82267efb19f5cdbcac9a3f3ed3a18922
Jonah Benares <jonahbenares.cenpri@gmail.com>
2019-10-21 14:13:40 +0800

    new db

--- db/db_tms.sql
+++ db/db_tms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2019 at 02:53 AM
+-- Generation Time: Oct 21, 2019 at 08:13 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,30 +23,196 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `company`
+--
+
+CREATE TABLE IF NOT EXISTS `company` (
+`company_id` int(11) NOT NULL,
+  `company_name` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`company_id`, `company_name`) VALUES
+(1, 'Central Negros Power Reliability Inc.'),
+(2, 'Energreen Power Inter-Island Corp.'),
+(3, 'PROGEN Deiseltech'),
+(4, 'Mindoro Harvest Energy Corp.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE IF NOT EXISTS `department` (
+`department_id` int(11) NOT NULL,
+  `department_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`department_id`, `department_name`) VALUES
+(1, 'Accounting Department'),
+(2, 'IT Department'),
+(3, 'Human Resource Department'),
+(4, 'Finance Department');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE IF NOT EXISTS `employees` (
+`employee_id` int(11) NOT NULL,
+  `employee_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `employee_name`) VALUES
+(1, 'Jonah Faye Benares'),
+(2, 'Hennelen Tanan'),
+(3, 'Mila Arana'),
+(4, 'Syndey Sinoro'),
+(5, 'Zyndyryn Rosales'),
+(6, 'Maylen Cabaylo'),
+(7, 'Stephine Severino'),
+(8, 'Jason Flor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_details`
+--
+
+CREATE TABLE IF NOT EXISTS `project_details` (
+`pd_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL DEFAULT '0',
+  `remarks` text,
+  `status_percentage` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `update_date` varchar(20) DEFAULT NULL,
+  `updated_by` int(11) NOT NULL DEFAULT '0',
+  `create_date` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_head`
+--
+
+CREATE TABLE IF NOT EXISTS `project_head` (
+`project_id` int(11) NOT NULL,
+  `start_date` varchar(20) DEFAULT NULL,
+  `completion_date` varchar(20) DEFAULT NULL,
+  `project_title` varchar(255) DEFAULT NULL,
+  `project_description` text,
+  `priority_no` int(11) NOT NULL DEFAULT '0' COMMENT '1 = top prio and so on',
+  `company_id` int(11) NOT NULL DEFAULT '0',
+  `department_id` int(11) NOT NULL DEFAULT '0',
+  `employee` varchar(100) DEFAULT NULL,
+  `status` int(11) DEFAULT '0' COMMENT '0=pending, 1=done, 2=cancelled',
+  `cancel_date` varchar(20) DEFAULT NULL,
+  `cancel_reason` text,
+  `create_date` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project_head`
+--
+
+INSERT INTO `project_head` (`project_id`, `start_date`, `completion_date`, `project_title`, `project_description`, `priority_no`, `company_id`, `department_id`, `employee`, `status`, `cancel_date`, `cancel_reason`, `create_date`) VALUES
+(1, '1970-01-01', '1970-01-01', 'Sample Project', '', 1, 1, 1, '4', 1, NULL, NULL, '2019-10-21 07:22:57'),
+(2, '1970-01-01', '1970-01-01', 'title1', '', 1, 1, 4, '0', 1, NULL, NULL, '2019-10-21 07:28:12'),
+(3, '1970-01-01', '1970-01-01', 'hello2', 'fgdfg', 1, 2, 4, '2, 8, 1, 4', 1, NULL, NULL, '2019-10-21 08:07:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reminders`
+--
+
+CREATE TABLE IF NOT EXISTS `reminders` (
+`reminder_id` int(11) NOT NULL,
+  `notes` text,
+  `employee_id` varchar(50) DEFAULT NULL,
+  `due_date` varchar(20) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0= active, 1= cancelled',
+  `cancel_date` varchar(20) DEFAULT NULL,
+  `cancel_reason` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
 `user_id` int(11) NOT NULL,
-  `fullname` varchar(100) DEFAULT NULL,
   `username` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `fullname`, `username`, `password`) VALUES
+INSERT INTO `users` (`user_id`, `username`, `password`, `fullname`) VALUES
 (1, 'admin', 'admin', 'admin'),
-(2, 'Hennelen Tanan', 'hen', 'hen'),
-(3, 'Jason Flor', 'flor', 'flor'),
-(4, 'Stephine David Severino', 'tep', 'tep'),
-(5, 'Imelda Espera', 'imelda', 'cenpri');
+(2, 'hen', 'hen', 'Hennelen Tanan'),
+(3, 'flor', 'flor', 'Jason Flor'),
+(4, 'tep', 'tep', 'Stephine David Severino'),
+(5, 'imelda', 'cenpri', 'Imelda Espera');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+ ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+ ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+ ADD PRIMARY KEY (`employee_id`);
+
+--
+-- Indexes for table `project_details`
+--
+ALTER TABLE `project_details`
+ ADD PRIMARY KEY (`pd_id`);
+
+--
+-- Indexes for table `project_head`
+--
+ALTER TABLE `project_head`
+ ADD PRIMARY KEY (`project_id`);
+
+--
+-- Indexes for table `reminders`
+--
+ALTER TABLE `reminders`
+ ADD PRIMARY KEY (`reminder_id`);
 
 --
 -- Indexes for table `users`
@@ -58,6 +224,36 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `project_details`
+--
+ALTER TABLE `project_details`
+MODIFY `pd_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `project_head`
+--
+ALTER TABLE `project_head`
+MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `reminders`
+--
+ALTER TABLE `reminders`
+MODIFY `reminder_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
