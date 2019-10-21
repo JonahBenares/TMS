2de03ed6a3d2ffb74c22e5a95c84109ebdb7aa56
Jonah Benares <jonahbenares.cenpri@gmail.com>
2019-10-21 14:12:04 +0800

    project head

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -43,11 +43,108 @@ class Task extends CI_Controller {
 
     public function add_task()
     {
-        $this->super_model->select_all_order_by("company", "company_name", ASC);
+        $project_id = $this->uri->segment(3);
+        $data['project_id'] = $project_id;
+        $data['company'] = $this->super_model->select_all_order_by("company", "company_name", "ASC");
+        $data['department'] = $this->super_model->select_all_order_by("department", "department_name", "ASC");
+        $data['employee'] = $this->super_model->select_all_order_by("employees", "employee_name", "ASC");
+
+        foreach($this->super_model->select_row_where("project_head", "project_id", $project_id) AS $proj){
+            $data['company_id']=$proj->company_id;
+            $data['department_id']=$proj->department_id;
+            $data['employee_id']=$proj->employee;
+            $data['start_date']=date('d / m / Y', strtotime($proj->start_date));
+            $data['completion_date']=date('d / m / Y', strtotime($proj->completion_date));
+            $data['priority_no']=$proj->priority_no;
+            $data['project_title']=$proj->project_title;
+            $data['project_desc']=$proj->project_description;
+
+        }
         $this->load->view('template/header');
         $this->load->view('template/navbar');
-        $this->load->view('task/add_task');
+        $this->load->view('task/add_task', $data);
         $this->load->view('template/footer');
+    }
+
+    public function insert_task(){
+
+        $rows_head = $this->super_model->count_rows("project_head");
+        if($rows_head==0){
+            $project_id=1;
+        } else {
+            $max = $this->super_model->get_max("project_head", "project_id");
+            $project_id = $max+1;
+        }
+
+        $start_date = date('Y-m-d', strtotime($this->input->post('start_date')));
+        $completion_date = date('Y-m-d', strtotime($this->input->post('completion_date')));
+        $project_title = utf8_encode($this->input->post('project_title'));
+        $project_desc = utf8_encode($this->input->post('project_desc'));
+        $create_date = date('Y-m-d H:i:s');
+        $emp = $this->input->post('employee');
+        $empid='';
+        $count= count($this->input->post('employee'));
+        for($x=0; $x<$count;$x++){
+            $empid .= $emp[$x].", ";
+        }
+        $empid = substr($empid, 0, -2);
+       
+       $data = array(
+            'project_id'=>$project_id,
+            'start_date'=>$start_date,
+            'completion_date'=>$completion_date,
+            'project_title'=>$project_title,
+            'project_description'=>$project_desc,
+            'priority_no'=>$this->input->post('priority_no'),
+            'company_id'=>$this->input->post('company'),
+            'department_id'=>$this->input->post('department'),
+            'employee'=>$empid,
+            'status'=>1,
+            'create_date'=>$create_date
+        );
+
+        if($this->super_model->insert_into("project_head", $data)){
+              $this->session->set_flashdata('msg', 'Project successfully added!');
+              redirect(base_url().'task/add_task/'.$project_id);
+        }
+    }
+
+    public function update_task(){
+
+        $project_id = $this->input->post('project_id');
+        $start_date = date('Y-m-d', strtotime($this->input->post('start_date')));
+        $completion_date = date('Y-m-d', strtotime($this->input->post('completion_date')));
+        $project_title = utf8_encode($this->input->post('project_title'));
+        $project_desc = utf8_encode($this->input->post('project_desc'));
+        $create_date = date('Y-m-d H:i:s');
+        $emp = $this->input->post('employee');
+        $empid='';
+        $count= count($this->input->post('employee'));
+        for($x=0; $x<$count;$x++){
+            $empid .= $emp[$x].", ";
+        }
+        $empid = substr($empid, 0, -2);
+
+         $data = array(
+            'project_id'=>$project_id,
+            'start_date'=>$start_date,
+            'completion_date'=>$completion_date,
+            'project_title'=>$project_title,
+            'project_description'=>$project_desc,
+            'priority_no'=>$this->input->post('priority_no'),
+            'company_id'=>$this->input->post('company'),
+            'department_id'=>$this->input->post('department'),
+            'employee'=>$empid,
+            'status'=>1,
+            'create_date'=>$create_date
+        );
+
+
+        if($this->super_model->update_where("project_head", $data, "project_id", $project_id)){
+              $this->session->set_flashdata('msg', 'Project successfully updated!');
+              redirect(base_url().'task/add_task/'.$project_id);
+        }
+
     }
 
     public function task_list()
--- application/views/task/add_task.php
+++ application/views/task/add_task.php
@@ -1,3 +1,4 @@
+
 <div class="page-wrapper">
     <div class="container-fluid">
         <div class="row page-titles">
@@ -23,61 +24,98 @@
                           <button class="tablinks" onclick="openCity(event, 'project_updates')">Project Updates</button>
                         </div>
                         <div id="add_project" class="tabcontent">
+                            <?php 
+                            if(empty($project_id)){
+                                $url = base_url().'task/insert_task';
+                            } else {
+                                $url = base_url().'task/update_task';
+                            } ?>
+                            <form method='POST' action='<?php echo $url; ?>'  onsubmit="return confirm('Do you really want to submit the form?');">
                             <div class="p-25">
                                 <div class="row">
+                                     <?php
+                                        $msg= $this->session->flashdata('msg');  
+                                        if($msg){
+                                     ?>
+                                     <div class="col-lg-6 offset-lg-3">
+                                        <div class="success bor-radius10 shadow alert-success alert-shake animated headShake" style='padding:10px'>
+                                            <center><?php echo $msg; ?></center>                    
+                                        </div>
+                                    </div>
+                                    <?php } ?>
+
                                     <div class="col-lg-6 offset-lg-3">
+                                  
+                                     
                                         <div class="form-group">
-                                            <select class="form-control">
-                                                <option value="">Select Company</option>
-                                                <option>123</option>
+                                            <select class="form-control" required name='company'>
+                                                <option value="">-Select Company-</option>
+                                                <?php foreach($company AS $com){ ?>
+                                                    <option value="<?php echo $com->company_id; ?>" <?php echo (!empty($project_id) ? (($company_id == $com->company_id) ? ' selected' : '') : ''); ?>><?php echo $com->company_name; ?></option>
+                                                <?php } ?>
                                             </select>
                                         </div>
                                         <div class="form-group">
-                                            <select class="form-control">
-                                                <option value="">Select Department</option>
-                                                <option>123</option>
+                                            <select class="form-control" name='department'>
+                                                <option value="">-Select Department-</option>
+                                                <?php foreach($department AS $dept){ ?>
+                                                    <option value="<?php echo $dept->department_id; ?>" <?php echo (!empty($project_id) ? (($department_id == $dept->department_id) ? ' selected' : '') : ''); ?>><?php echo $dept->department_name; ?></option>
+                                                <?php } ?>
                                             </select>
                                         </div>
                                         <div class="form-group">
-                                            <select class="form-control">
-                                                <option value="">Select Employee</option>
-                                                <option>123</option>
+                                            <select class="custom-select" multiple name="employee[]">
+                                                <option value="">-Select Accountable Employee-</option>
+                                                <?php foreach($employee AS $emp){ ?>
+
+                                                    <option value="<?php echo $emp->employee_id; ?>"
+                                                   <?php echo (!empty($employee_id) ? ((strstr( $employee_id, $emp->employee_id)) ? ' selected' : '') : ''); ?>
+                                                        ><?php echo $emp->employee_name; ?></option>
+                                                <?php } ?>
                                             </select>
+                                            
                                         </div>
                                         <div class="row">
                                             <div class="col-lg-6">
                                                 <div class="form-group">
-                                                    <input placeholder="Start Date" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date">
+                                                    <input placeholder="Start Date" class="form-control" name="start_date" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" value="<?php echo (!empty($project_id) ? $start_date : ''); ?>">
                                                 </div>
                                             </div>
                                             <div class="col-lg-6">
                                                 <div class="form-group">
-                                                    <input placeholder="Completion Date" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date">
+                                                    <input placeholder="Completion Date" class="form-control"  name="completion_date" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" value="<?php echo (!empty($project_id) ? $completion_date : ''); ?>">
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <input type="text" placeholder="Priority Number" class="form-control" name="">
+                                            <input type="number" placeholder="Priority Number" min='1' max='3'   class="form-control" required="required" name="priority_no" value="<?php echo (!empty($project_id) ? $priority_no : ''); ?>">
                                         </div>                                        
                                         <div class="form-group">                                            
-                                            <textarea placeholder="Project Title" class="form-control"></textarea>
+                                            <textarea placeholder="Project Title" name="project_title" class="form-control" required="required" ><?php echo (!empty($project_id) ? $project_title : ''); ?></textarea>
                                         </div>
                                         <div class="form-group">
-                                            <textarea placeholder="Project Description" class="form-control" rows="8"></textarea>
+                                            <textarea placeholder="Project Description" name="project_desc" class="form-control" rows="8"><?php echo (!empty($project_id) ? $project_desc : ''); ?></textarea>
                                         </div>
+                                        <?php if(empty($project_id)){ ?>
                                         <div class="form-group">
-                                            <input type="button" value="Save" class="btn btn-success btn-block" name="">
+                                            <input type="submit" value="Save" class="btn btn-success btn-block" name="">
                                         </div>
+                                       <?php } else { ?>
                                         <div class="row">
                                             <div class="col-lg-6">
-                                                    <input type="button" value="Add New Project" class="btn btn-primary btn-block" name="">
+                                                    <a href='<?php echo base_url(); ?>task/add_task' class="btn btn-primary btn-block">Add New Project</a>
                                             </div>
                                             <div class="col-lg-6">
-                                                    <input type="button" value="Update" class="btn btn-info btn-block" name="">
+                                                    <input type="submit" value="Update" class="btn btn-info btn-block" name="">
                                             </div>
                                         </div>
-                                        
+                                    <?php } ?>
+                                          <?php if(!empty($project_id)){ ?>
+                                        <input type='hidden' name='project_id' value="<?php echo $project_id; ?>">
+                                   <?php } ?>
                                     </div>
+                                  
+                                </form>
                                 </div>
                             </div>
                         </div>
@@ -89,15 +127,16 @@
                                         <h6>Start Date:</h6>
                                         <h6>Completion Date:</h6>
                                         <hr>
+                                          <div class="form-group">
+                                            <input placeholder="Update Date" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date">
+                                        </div> 
                                         <div class="form-group">
                                             <textarea class="form-control" rows="5" placeholder="Remarks"></textarea>
                                         </div>  
                                         <div class="form-group">
                                             <input type="number" class="form-control" name="" placeholder="Status Percentage">
                                         </div>  
-                                        <div class="form-group">
-                                            <input placeholder="Updated Date" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date">
-                                        </div> 
+                                      
                                         <div class="form-group">
                                             <input type="button" name="" class="btn btn-success btn-block"  value="Save Update">
                                         </div>
