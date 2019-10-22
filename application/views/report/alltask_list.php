3615b067922dfe0077407593bcd03707481b2ddf
Jonah Benares <jonahbenares.cenpri@gmail.com>
2019-10-22 15:01:29 +0800

    filter employee

--- application/views/report/alltask_list.php
+++ application/views/report/alltask_list.php
@@ -10,33 +10,42 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form method="POST" action = >
+            <form method="POST" action = "<?php echo base_url(); ?>report/search_all">
                 <div class="modal-body">                
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="form-group">
-                                <input placeholder="Start Date" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date" name="start_date">
+                                <input placeholder="Due Date: From" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date" name="start_date">
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="form-group">
-                                <input placeholder="Completion Date" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date" name="completion_date">
+                                <input placeholder="Due Date: To" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" id="date" name="completion_date">
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
                         <select name="company" class="form-control" placeholder="Company">
-                            <option value = "">--Select Company--</option>
+                             <option value = "">--Select Company--</option>
+                            <?php foreach($company AS $c){ ?>
+                            <option value = "<?php echo $c->company_id; ?>"><?php echo $c->company_name; ?></option>
+                            <?php } ?>
                         </select>
                     </div>
                     <div class="form-group">
                         <select name="department" class="form-control" placeholder="Department">
-                            <option value = "">--Select Department--</option>
+                              <option value = "">--Select Department--</option>
+                            <?php foreach($department AS $d){ ?>
+                            <option value = "<?php echo $d->department_id; ?>"><?php echo $d->department_name; ?></option>
+                            <?php } ?>
                         </select>
                     </div>
                     <div class="form-group">
-                        <select name="employee" class="form-control" placeholder="Employee" class="custom-select" multiple name="employee[]">
+                        <select class="form-control" placeholder="Employee" class="custom-select" multiple name="employee[]">
                             <option value = "">--Select Employee--</option>
+                            <?php foreach($employee AS $e){ ?>
+                            <option value = "<?php echo $e->employee_id; ?>"><?php echo $e->employee_name; ?></option>
+                            <?php } ?>
                         </select>
                     </div>
                     <div class="form-group">
@@ -89,8 +98,10 @@
                         </h4>        
                                         
                         <h6 class="card-subtitle"><br></h6>
-                        <div class="table-responsive">      
-                                <span class='btn btn-success disabled'>Filter Applied</span>names, <a href='<?php echo base_url(); ?>report/completed_list' class='remove_filter alert-link pull-right btn'><span class="fa fa-times"></span></a>
+                        <div class="table-responsive">     
+                          <?php if(!empty($filt)){ ?>      
+                                <span class='btn btn-success disabled'>Filter Applied</span><?php echo $filt; ?> <a href='<?php echo base_url(); ?>report/alltask_list' class='remove_filter alert-link pull-right btn'><span class="fa fa-times"></span></a>
+                             <?php } ?>           
                             <table id="myTable" class="table" >
                                 <thead >
                                     <tr class="nobor-top">
@@ -99,31 +110,58 @@
                                 </thead>
                                 <tbody>
                                     <!-- loop here -->
+                                      <?php foreach($projects AS $proj){ ?>
                                     <tr>
                                         <td class="p-0">
                                          
-                                            <a class="text-dfault"  href="<?php echo base_url(); ?>report/view_task/>" >
+                                            <a class="text-dfault"  href="<?php echo base_url(); ?>report/view_task/<?php echo $proj->project_id; ?>" >
                                                 <table width="100%" >
+                                                      <?php $employee = explode(", ", $proj->employee);  
+                                                     
+                                                        $count = count($employee);
+                                                        $emp='';
+                                                         for($x=0;$x<$count;$x++){
+                                                            $emp.= $ci->get_updated_name($employee[$x]). ", ";
+                                                         } 
+                                                         $employees = substr($emp, 0, -2);
+                                                          ?>
                                                     <tr>
                                                         <td width="6%">
+                                                            <?php if($proj->priority_no==1){ ?>
                                                             <span class="text-warning fa fa-flag"></span>
                                                             <span class="text-warning fa fa-flag"></span>
                                                             <span class="text-warning fa fa-flag"></span>
+                                                            <?php } else if($proj->priority_no==2){ ?>
+                                                            <span class="text-warning fa fa-flag"></span>
+                                                            <span class="text-warning fa fa-flag"></span>
+                                                            <?php } else if($proj->priority_no==3) { ?>
+                                                            <span class="text-warning fa fa-flag"></span>
+                                                            <?php } ?>
                                                         </td>
-                                                        <td class="bg-hovr" width="50%" class="nobor-top"><h4 class="proj-title m-0">Project Title</h4>
-                                                            <small class="proj-title">employee</small><br>
-                                                            <small class="proj-title"><b>Company</b></small>
+                                                        <td class="bg-hovr" width="50%" class="nobor-top"><h4 class="proj-title m-0"><?php echo $proj->project_title; ?></h4>
+                                                            <small class="proj-title"><?php echo $employees; ?></small><br>
+                                                            <small class="proj-title"><b><?php echo $ci->get_name("company", "company_name", "company_id", $proj->company_id); ?></b></small>
                                                         </td>
                                                         <td class="bg-hovr" width="%" class="nobor-top">
-                                                            <small class="proj-title btn-block m-t-5">START DATE: <span class="pull-right"></span></small>
-                                                            <small class="proj-title btn-block m-0">Due DATE: <span class="pull-right"></span></small>
-                                                            <small class="proj-title btn-block m-0">COMPLETED DATE: <span class="pull-right"></span></small>
-                                                            <small class="proj-title btn-block m-0">CANCELLED DATE: <span class="pull-right"></span></small>
+                                                            <small class="proj-title btn-block m-t-5">START DATE: <span class="pull-right"><?php echo date('m-d-Y', strtotime($proj->start_date)); ?></span></small>
+                                                            <small class="proj-title btn-block m-0">DUE DATE: <span class="pull-right"><?php echo date('m-d-Y', strtotime($proj->completion_date)); ?></span></small>
+                                                            <?php if($proj->status == 1) {  ?>
+                                                            <small class="proj-title btn-block m-0">COMPLETED DATE: <span class="pull-right"><?php echo date('m-d-Y', strtotime($ci->project_completed($proj->project_id))); ?></span></small>
+                                                            <?php } else if($proj->status == 2) { ?> 
+                                                            <small class="proj-title btn-block m-0">CANCELLED DATE: <span class="pull-right"><?php echo date('m-d-Y', strtotime($proj->cancel_date)); ?></span></small>
+                                                            <?php } ?>
                                                         </td>
 
                                                         <td class="bg-hovr" width="29%" class="nobor-top">
                                                             <div class="progress progress-bar-animated active">
-                                                                <div class="progress-bar bg-success progress-bar-striped" role="progressbar" aria-valuenow="50%" aria-valuemin="0" aria-valuemax="100" style="width: 50%"><h4 class="m-t-10 m-b-10">50%</h4></div>
+                                                                <?php if($proj->status == 0){
+                                                                    $bg= "bg-warning";
+                                                                } else if($proj->status == 1){
+                                                                     $bg= "bg-success";
+                                                                } else if($proj->status == 2){
+                                                                     $bg= "bg-danger";
+                                                                } ?>
+                                                                <div class="progress-bar <?php echo $bg; ?> progress-bar-striped" role="progressbar" aria-valuenow="<?php echo $ci->project_percent($proj->project_id); ?>%" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $ci->project_percent($proj->project_id); ?>%"><h4 class="m-t-10 m-b-10"><?php echo $ci->project_percent($proj->project_id); ?>%</h4></div>
                                                             </div>
                                                         </td>
                                                     </tr>
@@ -132,6 +170,7 @@
                                             
                                         </td>                                       
                                     </tr>
+                                     <?php } ?>
                                     <!-- loop here -->
                                 </tbody>
                             </table>
