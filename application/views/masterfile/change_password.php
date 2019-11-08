7ab1cab1f250780e8f852f817c1b2b129f5b47d2
stephinesev <stephineseverino.cenpri@gmail.com>
2019-11-08 16:21:49 +0800

    Fix change password

--- application/views/masterfile/change_password.php
+++ application/views/masterfile/change_password.php
@@ -27,7 +27,7 @@
                                        
                                       <div class='successmsg'></div>
                                         <div class="form-group">
-                                            <input type="text" name="old_password" id = "old_password" class="form-control" placeholder="Old Password">
+                                            <input type="password" name="old_password" id = "old_password" class="form-control" placeholder="Old Password">
                                              <div class='msgold'></div>
                                             <center><a href="" class="text-danger">Reset Password</a></center>
                                         </div>
@@ -39,6 +39,7 @@
                                             <div class='msg'></div>
                                         </div>
                                     </div>
+                                    <input type="hidden" name="baseurl" id = "baseurl" class="form-control" value="<?php echo base_url(); ?>">
                                     <input type="hidden" name="user_id" id = "user_id" class="form-control" value="<?php echo $userid; ?>">
                                     <div class="modal-footer">                                        
                                         <button type="submit" id='updatepw' class="btn btn-info btn-block" onclick="changepassword()">Update</button>
@@ -58,7 +59,7 @@
         var re_password = $('#re_password').val();
         var new_password = $('#new_password').val();
 
-        if(re_password!=new_password){
+        if(new_password!=re_password){
              $('div.msg').text('Error: Password did not match.');
              $('div.msg').addClass("success bor-radius10 shadow alert-danger alert-shake animated headShake");
              $('div.msg').attr("style", "padding:5px");
@@ -74,16 +75,16 @@
         var new_password = $('#new_password').val();
         var old_password = $('#old_password').val();
         var user_id = $('#user_id').val();
-     
+        var baseurl = $('#baseurl').val();
+        var redirect = baseurl+'index.php/masterfile/update_password';
         if(old_password==''){
-           
             $('div.msgold').text('Error: Old password must not be empty.');
-             $('div.msgold').addClass("success bor-radius10 shadow alert-danger alert-shake animated headShake");
-             $('div.msgold').attr("style", "padding:5px");
-             $("#updatepw").attr("disabled", true);
-        }  else {
+            $('div.msgold').addClass("success bor-radius10 shadow alert-danger alert-shake animated headShake");
+            $('div.msgold').attr("style", "padding:5px");
+            $("#updatepw").attr("disabled", true);
+        }else{
             $.ajax({
-                url: 'update_password',
+                url: redirect,
                 type: 'post',
                 data: {
                     'old_password' : old_password,
@@ -91,20 +92,19 @@
                     'user_id' : user_id
                 },
                 success: function(response){
-                  
-                  if (response == 'error' ) {
-                    $('div.msgold').text('Error: Old password incorrect.');
-                    $('div.msgold').addClass("success bor-radius10 shadow alert-danger alert-shake animated headShake");
-                    $('div.msgold').attr("style", "padding:10px");
-                    
-                  } else {
-                   $('div.successmsg').text('Password successfully changed.');
-                   $('div.successmsg').addClass("success bor-radius10 shadow alert-success");
-                   $('div.successmsg').attr("style", "padding:10px");
-                  
-                  }
+                    //alert(response);
+                    if (response == 'error') {
+                        $('div.msgold').text('Error: Old Password Incorrect.');
+                        $('div.msgold').addClass("success bor-radius10 shadow alert-danger alert-shake animated headShake");
+                        $('div.msgold').attr("style", "padding:10px");
+                    }else {
+                       $('div.successmsg').text('Password Successfully Changed.');
+                       $('div.successmsg').addClass("success bor-radius10 shadow alert-success");
+                       $('div.successmsg').attr("style", "padding:10px");
+                       $('div.msgold').hide();
+                    }
                 }
-              });
+            });
         }
     }
 </script>
